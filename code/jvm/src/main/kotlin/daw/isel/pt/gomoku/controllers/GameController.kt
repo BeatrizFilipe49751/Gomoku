3098705250629408228aad7763ff05654b78aeba
diogo <A49473@alunos.isel.pt>
2023-09-27 12:10:51 +0100

    Added empty controller and services files

---
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/controllers/GameController.kt
@@ -0,0 +1,7 @@
+package daw.isel.pt.gomoku.controllers
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GameController {
+}
---
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/services/GameServices.kt
@@ -0,0 +1,7 @@
+package daw.isel.pt.gomoku.services
+
+import org.springframework.stereotype.Component
+
+@Component
+class GameServices {
+}
