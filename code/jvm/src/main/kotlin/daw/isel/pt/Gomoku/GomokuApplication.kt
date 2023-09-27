2b219220243b07ae37961664e65feeb6b19fffd3
diogo <A49473@alunos.isel.pt>
2023-09-27 11:55:54 +0100

    Added springboot template to jvm folder

---
+++ code/jvm/src/main/kotlin/daw/isel/pt/Gomoku/GomokuApplication.kt
@@ -0,0 +1,11 @@
+package daw.isel.pt.Gomoku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GomokuApplication
+
+fun main(args: Array<String>) {
+	runApplication<GomokuApplication>(*args)
+}
---
+++ code/jvm/src/test/kotlin/daw/isel/pt/Gomoku/GomokuApplicationTests.kt
@@ -0,0 +1,13 @@
+package daw.isel.pt.Gomoku
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GomokuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
