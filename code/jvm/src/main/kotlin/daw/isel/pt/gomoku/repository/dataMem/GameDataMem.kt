018fe33211f7fdb4cee051eb901041c9944291af
49473 - Diogo Carichas <49473@alunos.isel.pt>
2023-09-29 11:48:10 +0100

    Updated dataMem

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/dataMem/GameDataMem.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/dataMem/GameDataMem.kt
@@ -1,6 +1,16 @@
 package daw.isel.pt.gomoku.repository.dataMem
 
+import daw.isel.pt.gomoku.domain.Game
+import daw.isel.pt.gomoku.domain.User
 import daw.isel.pt.gomoku.repository.interfaces.GameRepository
+import java.util.*
 
 class GameDataMem: GameRepository {
+    override fun getGame(id: UUID): Game {
+        TODO("Not yet implemented")
+    }
+
+    override fun createGame(name: String, playerWhite: User, playerBlack: User): UUID {
+        TODO("Not yet implemented")
+    }
 }
--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/dataMem/UsersDataMem.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/dataMem/UsersDataMem.kt
@@ -1,6 +1,14 @@
 package daw.isel.pt.gomoku.repository.dataMem
 
+import daw.isel.pt.gomoku.domain.User
 import daw.isel.pt.gomoku.repository.interfaces.UserRepository
 
 class UsersDataMem: UserRepository {
+    override fun getUser(id: Int): User {
+        TODO("Not yet implemented")
+    }
+
+    override fun createUser(username: String): Int {
+        TODO("Not yet implemented")
+    }
 }
