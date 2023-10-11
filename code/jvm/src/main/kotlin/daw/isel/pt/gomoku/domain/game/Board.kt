696ffa179314b4779bbae87369422e8d6d29ba64
49473 - Diogo Carichas <49473@alunos.isel.pt>
2023-10-11 13:06:00 +0100

    Changed Board class

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/game/Board.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/game/Board.kt
@@ -5,5 +5,6 @@ const val BOARD_DIM = 15
 const val LAST_INDEX = BOARD_DIM - 1
 const val LAST_COL_CHAR = 'a' + LAST_INDEX
 const val MAX_POSITIONS = BOARD_DIM * BOARD_DIM
-class Board(val positions : List<Position>){
+data class Board(val positions : List<Position> = Position.positions) {
+    //TODO: Game Logic
 }
