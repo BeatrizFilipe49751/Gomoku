37182021b6c32f47a4b480290c468cf636b7724d
49473 - Diogo Carichas <49473@alunos.isel.pt>
2023-10-11 11:52:53 +0100

    Added Column and values to board

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/Board.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/Board.kt
@@ -2,6 +2,9 @@ package daw.isel.pt.gomoku.domain
 
 
 const val BOARD_DIM = 15
+const val LAST_ROW_INDEX = BOARD_DIM - 1
+const val LAST_COL_CHAR = 'a' + LAST_ROW_INDEX
+
 class Board{
 
 }
---
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/Column.kt
@@ -0,0 +1,20 @@
+package daw.isel.pt.gomoku.domain
+
+data class Column(val symbol: Char) {
+    val index: Int = symbol - 'a'
+
+    companion object {
+        val columns = ('a' .. LAST_COL_CHAR).map {idx -> Column(idx) }
+        operator fun invoke(s: Char): Column {
+            require(s in 'a' .. LAST_COL_CHAR) { "Symbol must be between 'a' and '$LAST_COL_CHAR'" }
+            val index = s - 'a'
+            return columns[index]
+        }
+    }
+}
+
+fun Char.toColumnOrNull() =
+    Column.columns.find { it.symbol == this }
+
+
+fun Int.indexToColumn() = Column.columns.find { it.index == this }
