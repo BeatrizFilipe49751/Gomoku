fddaf46b13a2a911333060b248f428609175eef0
49473 - Diogo Carichas <49473@alunos.isel.pt>
2023-10-11 11:59:42 +0100

    Changed GameState and added PieceColor

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/game/GameState.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/game/GameState.kt
@@ -1,6 +1,6 @@
 package daw.isel.pt.gomoku.domain.game
 
-enum class GameState {
-    ACTIVE,
-    FINISHED
+enum class GameState(val state: Char) {
+    ACTIVE('A'),
+    FINISHED('F')
 }
---
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/domain/game/PieceColor.kt
@@ -0,0 +1,5 @@
+package daw.isel.pt.gomoku.domain.game
+
+enum class PieceColor(val color: Char) {
+    BLACK('b'), WHITE('w')
+}
