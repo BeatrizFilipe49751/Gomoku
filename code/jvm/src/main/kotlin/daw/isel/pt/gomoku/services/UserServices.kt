e2b9eb4079296d7222f2835769efcd63dfa735f8
Alexandre1303 <alexandreseverino2003@gmail.com>
2023-10-12 21:48:26 +0100

    Updated services to use transaction manager

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/services/UserServices.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/services/UserServices.kt
@@ -2,6 +2,7 @@ package daw.isel.pt.gomoku.services
 
 
 import daw.isel.pt.gomoku.domain.User
+import daw.isel.pt.gomoku.repository.interfaces.TransactionManager
 import daw.isel.pt.gomoku.repository.interfaces.UserRepository
 import daw.isel.pt.gomoku.services.exceptions.InvalidCredentialsException
 import daw.isel.pt.gomoku.services.exceptions.NotFoundException
@@ -10,21 +11,32 @@ import org.springframework.stereotype.Component
 import java.util.UUID
 
 @Component
-class UserServices(val userRepo: UserRepository) {
-    fun getUser(id: Int) = userRepo.getUser(id) ?: throw NotFoundException("User not found")
+class UserServices(val transactionManager: TransactionManager) {
+    fun getUser(id: Int): User =
+        transactionManager.run {
+            it.usersRepository.getUser(id) ?: throw NotFoundException("User not found")
+        }
+
     fun createUser(username: String?, email: String?): User {
         if (username.isNullOrEmpty() || email.isNullOrEmpty()) throw InvalidCredentialsException("Username or email missing")
         if(!email.contains("@")) throw InvalidCredentialsException("Username or email missing")
         val newToken = UUID.randomUUID().toString()
-        return userRepo.createUser(username, email, newToken)
+        return transactionManager.run {
+            it.usersRepository.createUser(username, email, newToken)
+        }
     }
 
     fun createLobby(userId: Int): Int {
-        return userRepo.createLobby(userId)
+        return transactionManager.run {
+            it.usersRepository.createLobby(userId)
+        }
     }
 
     fun checkUserToken(token: String): User? {
-        return userRepo.checkUserToken(token)
+        return transactionManager.run {
+            it.usersRepository.checkUserToken(token)
+        }
+
     }
 
 }
