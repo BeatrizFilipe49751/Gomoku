a8b7252e514efdfc57d1eb665c5c3c68f58bbc18
Alexandre1303 <alexandreseverino2003@gmail.com>
2023-10-17 20:32:53 +0100

    altered game repo

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/interfaces/GameRepository.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/interfaces/GameRepository.kt
@@ -1,10 +1,11 @@
 package daw.isel.pt.gomoku.repository.interfaces
 
+import daw.isel.pt.gomoku.controllers.models.GameSerialized
 import daw.isel.pt.gomoku.domain.game.Game
 
 interface GameRepository {
-    fun getGame(gameId: String): Game
-    fun createGame(game: Game, playerBlack: Int, playerWhite: Int): Game
+    fun getGame(gameId: String): GameSerialized
+    fun createGame(game: Game, playerBlack: Int, playerWhite: Int): GameSerialized
 
-    fun updateGame(gameId: String): Game
+    fun updateGame(gameId: String): GameSerialized
 }
