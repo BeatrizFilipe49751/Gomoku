67fab5ee32b735dcef438871bb5d184916471e07
49473 - Diogo Carichas <49473@alunos.isel.pt>
2023-10-17 20:32:07 +0100

    Changed Game data

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/interfaces/GameRepository.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/repository/interfaces/GameRepository.kt
@@ -1,12 +1,10 @@
 package daw.isel.pt.gomoku.repository.interfaces
 
 import daw.isel.pt.gomoku.domain.game.Game
-import daw.isel.pt.gomoku.domain.User
-import java.util.*
 
 interface GameRepository {
     fun getGame(gameId: String): Game
-    fun createGame(gameId: String, name: String, playerWhite: User, playerBlack: User): Game
+    fun createGame(game: Game, playerBlack: Int, playerWhite: Int): Game
 
     fun updateGame(gameId: String): Game
 }
--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/services/GameServices.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/services/GameServices.kt
@@ -1,20 +1,47 @@
 package daw.isel.pt.gomoku.services
 
-import daw.isel.pt.gomoku.domain.game.Board
-import daw.isel.pt.gomoku.domain.game.Game
-import daw.isel.pt.gomoku.domain.game.Piece
+import daw.isel.pt.gomoku.domain.game.*
 import daw.isel.pt.gomoku.repository.interfaces.GameRepository
 import daw.isel.pt.gomoku.repository.interfaces.transactions.TransactionManager
+import daw.isel.pt.gomoku.services.exceptions.AlreadyInLobbyException
+import daw.isel.pt.gomoku.services.exceptions.InvalidCredentialsException
+import daw.isel.pt.gomoku.services.exceptions.LobbyErrorMessages
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class GameServices(private val transactionManager: TransactionManager) {
 
-    fun createGame(name: String) {
-        //data.createGame(Game(UUID.randomUUID(), Board(), name))
+    fun createGame(name: String, playerBlack: Int, playerWhite: Int): Game {
+        return transactionManager.run {
+            it.gameRepository.createGame(
+                Game(UUID.randomUUID(), Board(), name),
+                playerBlack,
+                playerWhite
+            )
+        }
     }
-    fun play(pieceToPlay: Piece) {
 
+    fun getGame(gameId: String): Game {
+        return transactionManager.run {
+            it.gameRepository.getGame(gameId)
+        }
+    }
+
+    fun play(gameId: String, pieceToPlay: Piece) {
+        transactionManager.run {
+            val game = it.gameRepository.getGame(gameId)
+        }
+        /*
+        check(state == GameState.ACTIVE) { "Game has finished" }
+        check(currentTurn == pieceToPlace.color) { "Not your turn" }
+        check(!board.hasPiece(pieceToPlace)) { "Piece already there" }
+        val newBoard = board.copy(pieces = board.pieces + pieceToPlace)
+        if (newBoard.pieces.size < WIN_STREAK)
+            return copy(board = newBoard, currentTurn = currentTurn.switchTurn() )
+        return if (checkWin(newBoard, pieceToPlace))
+            copy(board = newBoard, state = GameState.FINISHED)
+        else copy(board = newBoard, currentTurn = currentTurn.switchTurn() )
+         */
     }
 }
