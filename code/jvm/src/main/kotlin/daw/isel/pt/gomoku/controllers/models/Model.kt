04d35cd8229d6b90b3dea1869efc0e81bb926bbb
Alexandre1303 <alexandreseverino2003@gmail.com>
2023-10-17 20:33:00 +0100

    altered game repo

--- code/jvm/src/main/kotlin/daw/isel/pt/gomoku/controllers/models/Model.kt
+++ code/jvm/src/main/kotlin/daw/isel/pt/gomoku/controllers/models/Model.kt
@@ -1,8 +1,12 @@
 package daw.isel.pt.gomoku.controllers.models
 
+import daw.isel.pt.gomoku.domain.game.Board
+
 data class UserOut(val userId: Int, val username: String, val email: String)
 data class UserOutWithToken(val userId: Int, val username: String, val token: String)
 data class UserIn(val username: String?, val email: String?)
 
 data class LobbyIn(val name: String?)
+
+data class GameSerialized(val id: String, val name: String, val board: String, val state: Char)
 data class ErrorResponse(val status: Int, val message: String = "No message provided")
