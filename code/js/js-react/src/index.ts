809ac2b8050d77415e7423cd8568fc1602049bef
Alexandre1303 <a49457@alunos.isel.pt>
2023-12-05 22:15:28 +0000

    Added docker compose file and organized directories

--- code/js/js-react/src/index.ts
+++
@@ -1,6 +0,0 @@
-//import {demo} from './assets/tsx/fetch'
-//import {demo} from './assets/tsx/fetchButton'
-//import {main} from './assets/tsx/counter'
-import {appRouter} from "./assets/tsx/router";
-
-appRouter()
