a0980f68a6510b207400603998a1caeedaec2264
49473 - Diogo Carichas <49473@alunos.isel.pt>
2023-12-07 16:46:08 +0000

    changed static folder structure

--- code/gomoku_web_app/js/js-react/src/assets/tsx/router.tsx
+++ code/gomoku_web_app/js/js-react/src/assets/tsx/router.tsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Outlet, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import '../css/App.css';
 
-import Home from './home';
-import Login from './login';
-import Register from './register';
-import Navbar from './navbar';
-import Leaderboard from './leaderboard';
+import Home from './web-ui/home';
+import Login from './user/login';
+import Register from './user/register';
+import Navbar from './web-ui/navbar';
+import Leaderboard from './user/leaderboard';
 
 const HeaderLayout = () => (
   <>
